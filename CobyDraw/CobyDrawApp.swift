c0a4c34d87954903796231585f24b44da31c6975
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-08-02 14:25:05 +0900

    Initial Commit

---
+++ CobyDraw/CobyDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  CobyDrawApp.swift
+//  CobyDraw
+//
+//  Created by COBY_PRO on 2023/08/02.
+//
+
+import SwiftUI
+
+@main
+struct CobyDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
